e6f5518e0c64944a7ef3bc7194f863da7a6ebd2c
Avi <98209679+up-the-hill@users.noreply.github.com>
2026-08-03 21:40:33 +1000

    Add: zigzag-conversion - submission-1

---
+++ Data Structures & Algorithms/zigzag-conversion/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        rows = [''] * (numRows)
+        rowPos = 0
+        reversing = False
+        for i, c in enumerate(s):
+            rows[rowPos] += c
+            if reversing:
+                if rowPos == 0:
+                    rowPos += 1
+                    reversing = False
+                else:
+                    rowPos -= 1
+            else:
+                if rowPos == numRows - 1:
+                    rowPos -= 1
+                    reversing = True
+                else:
+                    rowPos += 1
+        return ''.join(rows)
+
+
